d5740b774d22162a5b0a04504b377154025353cd
J. Prajapati <jignesh.prajapati@aloissolutions.com>
2024-12-05 10:46:59 +0530

    FIRST commit

---
+++ index.py
@@ -0,0 +1,4 @@
+from llama_index import VectorStoreIndex,SimpleDirectoryReader
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
---
+++ install.sh
@@ -0,0 +1,5 @@
+# custom selection of integrations to work with core
+pip install llama-index-core
+pip install llama-index-llms-openai
+pip install llama-index-llms-replicate
+pip install llama-index-embeddings-huggingface
